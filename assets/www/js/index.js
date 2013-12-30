d678b913fdfedee82cd2438a20564fb86c70ced6
bluecasper477 <bluecasper477@gmail.com>
2013-12-30 16:34:19 -0500

    initial commit of baseline usless working code

--- assets/www/js/index.js
+++ assets/www/js/index.js
@@ -2,5 +2,11 @@ var app = {
     initialize: function() {
         // TODO: put PhoneGap initialization here
         // alert("Hello World!");
+		$('#submit').click(function(){
+			var user = $('#user').val();
+			var pass = $('#pass').val();
+			var brand = $('#brand').val();
+			alert('Username: '+user+', Password: '+pass+', Brand: '+brand);
+		});
     },
 };
